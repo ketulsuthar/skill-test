d75ac7218a28c0fdce75a352c3082fec48a4b165
ketulsuthar <k2lsuthar@gmail.com>
2021-12-11 00:19:53 -0500

    Added code for set and retrieve user from localstorage

---
+++ javascript/user.js
@@ -0,0 +1,38 @@
+
+/*
+Write JavaScript to store and retrieve a JSON file on your local storage with the following information.
+
+            Name: John Doe
+
+            Age: 35
+
+            City: Collingwood
+*/
+
+const user = {
+    name: "John Doe",
+    age: 35,
+    city: "Collingwood"
+}
+
+/*
+Set user in local storage
+*/
+setUser = (user) => {
+    if (typeof(Storage) !== "undefined") {
+        window.localStorage.setItem('user', JSON.stringify(person));
+    } else {
+        console.log("localstorage not supported by current browser")
+    }
+}
+
+/*
+Return user from local storage
+*/
+getUser = () => {
+    let user = null
+    if (typeof(Storage) !== "undefined") {
+        user = JSON.parse(window.localStorage.getItem('user'));
+    }
+    return user
+}
